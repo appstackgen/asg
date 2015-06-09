f467cde6aa5cb7c050fc034539fdd0ee2d824702
Johannes Lochmann <appstackgen@gmail.com>
2015-06-09 08:54:03 +0200

    added types.h

---
+++ src/lib/asg/kernel/types.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015, Johannes Lochmann - johannes.lochmann@gmail.com
+ *
+ * All rights reserved.
+ */
+
+#pragma once
+
+#include <string>
+#include <sstream>
+
+namespace asg {
+}
